9c4476345b7883fb26e24e9fec751ef87970804b
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2026-06-15 00:10:37 -0400

    Update javascript.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- messages/de/javascript.php
+++ messages/de/javascript.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'Cache cleared. Reloading…' => '',
-    'Could not reach the API. Please try again later or contact support.' => '',
-    'Failed to clear cache.' => '',
-    'Testing connection…' => '',
+    'Cache cleared. Reloading…' => 'Cache geleert. Wird neu geladen …',
+    'Could not reach the API. Please try again later or contact support.' => 'Die API konnte nicht erreicht werden. Bitte versuchen Sie es später erneut oder kontaktieren Sie den Support.',
+    'Failed to clear cache.' => 'Cache konnte nicht geleert werden.',
+    'Testing connection…' => 'Verbindung wird getestet …',
 ];
